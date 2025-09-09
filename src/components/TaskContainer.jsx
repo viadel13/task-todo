4e637ea99c70f8e77f81fda544f42d24eca16812
viadel13 <shababizetre@gmail.com>
2025-09-09 13:02:39 +0100

    nb

--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,55 +1,60 @@
 //Ce composant est utilisé pour afficher l'intégralité de la fonction tache
-import { Header} from "./header/header";
+import { Header } from "./header/header";
 import { TaskInput } from "./TaskInput/taskInput";
 import { TaskList } from "./TaskList/taskList";
-import  {Footer} from "./Footer/footer";
+import { Footer } from "./Footer/footer";
 import { useState } from "react";
-export const TaskContainer = ()=>{
-    const[tasksList, setTasksList]=useState([
-        
-    ]);
-    console.log("taskList:", tasksList);
-      const addTask =(title)=>{
-        const newTask ={
-            id: tasksList.length? tasksList[tasksList.lenght - 1].id + 1:1, // permet de donner le nombre d'élément ajouté
-            title: title,
-            completed: false,
+export const TaskContainer = () => {
+  const [tasksList, setTasksList] = useState([]);
+  console.log("taskList:", tasksList);
+  const addTask = (title) => {
+    const newTask = {
+      id: tasksList.length ? tasksList[tasksList.length - 1].id + 1 : 1, // permet de donner le nombre d'élément ajouté
+      title: title,
+      completed: false,
+    };
+    const updatedTasks = [...tasksList, newTask];
+    localStorage.setItem("task-todo", JSON.stringify(updatedTasks));
+    setTasksList([...tasksList, newTask]); // permet d'ajouter une nouvelle tâche la liste
+  };
 
-        };
-        setTasksList([...tasksList, newTask]);// permet d'ajouter une nouvelle tâche la liste
-    };
-        const editTask= (id, completedValue)=>{
-        setTasksList(
-            tasksList.map((task)=>// .map permet d'afficher les éléments d'un tableau dynamiquement
-                task.id === id?{...task, completed:completedValue} : task
-                )
-        );
-    };
-        const deleteTask=(id)=> {
-            setTasksList(tasksList.filter((task)=> task.id!== id));
-        }
-    const getTaskCount=()=> {
-            const completedTasks = tasksList.filter((task)=> task.completed ).length;
-            const incompletedTasks = tasksList.length- completedTasks;
-            return{
-                completedTasks,
-                incompletedTasks,
-            };
-            };
-            const {completedTasks, incompletedTasks}= getTaskCount();
-            console.log(completedTasks, incompletedTasks);
-    
-        return(
-        <main>
-            <Header/>
-            <TaskInput addTask={addTask}/> {/*permet d'envoyer la methode à utiliser à ce composant enfant*/}
-            <TaskList tasksList={tasksList} 
-                      editTasK={editTask} 
-                      deleteTask={deleteTask} 
-                      incompletedTasks={incompletedTasks}/>
-            <Footer completedTasks={completedTasks}/>
-        </main>
+  const editTask = (id, completedValue) => {
+    setTasksList(
+      tasksList.map(
+        (
+          task, // .map permet d'afficher les éléments d'un tableau dynamiquement
+        ) => (task.id === id ? { ...task, completed: completedValue } : task),
+      ),
     );
+  };
+  const deleteTask = (id) => {
+    setTasksList(tasksList.filter((task) => task.id !== id));
+  };
+  const getTaskCount = () => {
+    const completedTasks = tasksList.filter((task) => task.completed).length;
+    const incompletedTasks = tasksList.length - completedTasks;
+    return {
+      completedTasks,
+      incompletedTasks,
+    };
+  };
+  const { completedTasks, incompletedTasks } = getTaskCount();
+  console.log(completedTasks, incompletedTasks);
+
+  return (
+    <main>
+      <Header />
+      <TaskInput addTask={addTask} />{" "}
+      {/*permet d'envoyer la methode à utiliser à ce composant enfant*/}
+      <TaskList
+        tasksList={tasksList}
+        editTasK={editTask}
+        deleteTask={deleteTask}
+        incompletedTasks={incompletedTasks}
+      />
+      <Footer completedTasks={completedTasks} />
+    </main>
+  );
 };
 
 export default TaskContainer;
--- src/components/TaskList/taskList.jsx
+++ src/components/TaskList/taskList.jsx
@@ -1,38 +1,57 @@
 import { TaskItem } from "../TaskItem/taskItem";
 import styles from "./taskList.module.css";
-export const TaskList= ({tasksList,editTasK,deleteTask,incompletedTasks,})=>{
+export const TaskList = ({
+  tasksList,
+  editTasK,
+  deleteTask,
+  incompletedTasks,
+}) => {
+  const taskLocal = localStorage.getItem("task-todo");
 
-    const taskList = tasksList.map((task) => (
-        <TaskItem key={task.id} task={task} editTask={editTasK} deleteTask={deleteTask}/>
-    ));
-    if(taskList && taskList.length > 0){
-        return(
-         <div className="box">
-            <h2 className={styles.title}>
+  console.log(tasksList);
 
-            {incompletedTasks >0 &&(
-           <>👉Il te reste encore <span className="important">{incompletedTasks} </span> tâches à accomplir </>  
-        )}
+  const tasks = tasksList.length
+    ? tasksList
+    : taskLocal
+      ? JSON.parse(taskLocal)
+      : [];
 
-            {incompletedTasks === 0 && (
-                <> 👉  Génial, tu as accompli toutes tes tâches</>
-            )}
-             </h2>
-
-            {tasksList && tasksList.length > 0 &&(
-            <ul className={styles.container}>
-        {taskList}           
-         </ul>
-            )}
-        </div>
-        );
-    }
+  const taskList = tasks.map((task) => (
+    <TaskItem
+      key={task.id}
+      task={task}
+      editTask={editTasK}
+      deleteTask={deleteTask}
+    />
+  ));
+  if (taskList && taskList.length > 0) {
     return (
-        <div className="box">
-            <h2 className={styles.emptyState}>
-               👋 Salut, Tu n'as rien à faire. Profite de ton temps libre!
-            </h2>
-        </div>
-       
+      <div className="box">
+        <h2 className={styles.title}>
+          {incompletedTasks > 0 && (
+            <>
+              👉Il te reste encore{" "}
+              <span className="important">{incompletedTasks} </span> tâches à
+              accomplir{" "}
+            </>
+          )}
+
+          {incompletedTasks === 0 && (
+            <> 👉 Génial, tu as accompli toutes tes tâches</>
+          )}
+        </h2>
+
+        {tasksList && tasksList.length > 0 && (
+          <ul className={styles.container}>{taskList}</ul>
+        )}
+      </div>
     );
+  }
+  return (
+    <div className="box">
+      <h2 className={styles.emptyState}>
+        👋 Salut, Tu n'as rien à faire. Profite de ton temps libre!
+      </h2>
+    </div>
+  );
 };
